957771ac7aa16a64cb7806989b414d7999ff5559
root <nagaraja.hm.0001@gmail.com>
2022-01-02 23:00:33 +0100

    Update: submodule StbM

---
+++ LATEST/StbM.cpp
@@ -0,0 +1,7 @@
+#include "StbM.h"
+
+class_StbM StbM;
+
+void class_StbM::InitFunction(void){
+}
+
---
+++ LATEST/StbM.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_StbM{
+   public:
+      void InitFunction(void);
+};
+
+extern class_StbM StbM;
+
