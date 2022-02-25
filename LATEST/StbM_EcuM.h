cf087ed0c7f057b54baf4955128a326158280de9
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:08:31 +0100

    Update: submodule StbM

--- LATEST/StbM_EcuM.h
+++ LATEST/StbM_EcuM.h
@@ -27,7 +27,8 @@ class interface_StbM_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, STBM_CODE) InitFunction(void);
+      virtual FUNC(void, STBM_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, STBM_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
