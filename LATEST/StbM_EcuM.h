164a195f56059058adc1d903c550584c3cff0422
root <nagaraja.hm.0001@gmail.com>
2022-03-09 12:55:43 +0100

    Update: submodule StbM

--- LATEST/StbM_EcuM.h
+++ LATEST/StbM_EcuM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infEcuMClient *gptrinfEcuMClient_StbM;
+extern infEcuMClient* gptrinfEcuMClient_StbM;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
--- LATEST/StbM_SchM.h
+++ LATEST/StbM_SchM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infSchMClient *gptrinfSchMClient_StbM;
+extern infSchMClient* gptrinfSchMClient_StbM;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
