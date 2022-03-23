e2049e31a90f49d8afdee00e735b9bcf9bbcc554
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:09:23 +0100

    Update: AUTOSAR basic software service module StbM

--- LATEST/StbM.cpp
+++ LATEST/StbM.cpp
@@ -40,6 +40,16 @@ class module_StbM:
       FUNC(void, STBM_CODE) DeInitFunction (void);
       FUNC(void, STBM_CODE) GetVersionInfo (void);
       FUNC(void, STBM_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, STBM_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
