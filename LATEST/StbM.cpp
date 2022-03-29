0f0f3ae900112f221b176b8bbc6c9d7c62d98484
root <nagaraja.hm.0001@gmail.com>
2022-03-29 13:49:12 +0200

    Update: AUTOSAR basic software service module StbM

--- LATEST/StbM.cpp
+++ LATEST/StbM.cpp
@@ -7,7 +7,6 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "module.hpp"
-#include "CfgStbM.hpp"
 #include "infStbM_EcuM.hpp"
 #include "infStbM_Dcm.hpp"
 #include "infStbM_SchM.hpp"
@@ -36,37 +35,40 @@ class module_StbM:
       public abstract_module
 {
    public:
+      module_StbM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
+      }
       FUNC(void, STBM_CODE) InitFunction   (void);
       FUNC(void, STBM_CODE) DeInitFunction (void);
-      FUNC(void, STBM_CODE) GetVersionInfo (void);
       FUNC(void, STBM_CODE) MainFunction   (void);
-
-   private:
-      CONST(Std_TypeVersionInfo, STBM_CONST) VersionInfo = {
-            0x0000
-         ,  0xFFFF
-         ,  0x01
-         ,  '0'
-         ,  '1'
-         ,  '0'
-      };
 };
+
+extern VAR(module_StbM, STBM_VAR) StbM;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
 /******************************************************************************/
+CONSTP2VAR(infEcuMClient, STBM_VAR, STBM_CONST) gptrinfEcuMClient_StbM = &StbM;
+CONSTP2VAR(infDcmClient,  STBM_VAR, STBM_CONST) gptrinfDcmClient_StbM  = &StbM;
+CONSTP2VAR(infSchMClient, STBM_VAR, STBM_CONST) gptrinfSchMClient_StbM = &StbM;
 
 /******************************************************************************/
 /* PARAMS                                                                     */
 /******************************************************************************/
+#include "CfgStbM.hpp"
 
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_StbM, STBM_VAR) StbM;
-CONSTP2VAR(infEcuMClient, STBM_VAR, STBM_CONST) gptrinfEcuMClient_StbM = &StbM;
-CONSTP2VAR(infDcmClient,  STBM_VAR, STBM_CONST) gptrinfDcmClient_StbM  = &StbM;
-CONSTP2VAR(infSchMClient, STBM_VAR, STBM_CONST) gptrinfSchMClient_StbM = &StbM;
+VAR(module_StbM, STBM_VAR) StbM(
+   {
+         0x0000
+      ,  0xFFFF
+      ,  0x01
+      ,  '0'
+      ,  '1'
+      ,  '0'
+   }
+);
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
@@ -77,14 +79,6 @@ FUNC(void, STBM_CODE) module_StbM::InitFunction(void){
 
 FUNC(void, STBM_CODE) module_StbM::DeInitFunction(void){
    StbM.IsInitDone = E_NOT_OK;
-}
-
-FUNC(void, STBM_CODE) module_StbM::GetVersionInfo(void){
-#if(STD_ON == StbM_DevErrorDetect)
-//TBD: API parameter check
-   Det_ReportError(
-   );
-#endif
 }
 
 FUNC(void, STBM_CODE) module_StbM::MainFunction(void){
