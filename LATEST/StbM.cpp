cb59bdd01e92a482d31467507ce0c5f8ddbe7c05
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:05:21 +0200

    Update: AUTOSAR basic software service module StbM

--- LATEST/StbM.cpp
+++ LATEST/StbM.cpp
@@ -37,10 +37,9 @@ class module_StbM:
    public:
       module_StbM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, STBM_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, STBM_CONFIG_DATA, STBM_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, STBM_CODE) InitFunction   (void);
       FUNC(void, STBM_CODE) DeInitFunction (void);
       FUNC(void, STBM_CODE) MainFunction   (void);
 };
@@ -77,23 +76,39 @@ VAR(module_StbM, STBM_VAR) StbM(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, STBM_CODE) module_StbM::InitFunction(
-   CONSTP2CONST(CfgStbM_Type, CFGSTBM_CONFIG_DATA, CFGSTBM_APPL_CONST) lptrCfgStbM
+   CONSTP2CONST(CfgModule_TypeAbstract, STBM_CONFIG_DATA, STBM_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgStbM){
+   if(E_OK == IsInitDone){
 #if(STD_ON == StbM_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgStbM for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == StbM_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_StbM as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   StbM.IsInitDone = E_OK;
 }
 
 FUNC(void, STBM_CODE) module_StbM::DeInitFunction(void){
-   StbM.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == StbM_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, STBM_CODE) module_StbM::MainFunction(void){
