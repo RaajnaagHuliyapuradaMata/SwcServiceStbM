67d1e0f0f2a4e52f535f501ef3472a6a67c77244
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:26:16 +0000

    Update: AUTOSAR basic software service module StbM

--- LATEST/StbM.cpp
+++ LATEST/StbM.cpp
@@ -101,8 +101,8 @@ FUNC(void, STBM_CODE) module_StbM::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgStbM;
          }
       }
       IsInitDone = E_OK;
