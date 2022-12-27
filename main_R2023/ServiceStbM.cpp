828a130b969b0bba3d5d8ae6f5de0df7c5e2979e
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-12-27 20:37:06 +0100

    Update: AUTOSAR basic software service module StbM

---
+++ main_R2023/ServiceStbM.cpp
@@ -0,0 +1,145 @@
+/******************************************************************************/
+/* File   : ServiceStbM.cpp                                                   */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "ServiceStbM.hpp"
+#include "infServiceStbM_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define SERVICESTBM_AR_RELEASE_VERSION_MAJOR                                   4
+#define SERVICESTBM_AR_RELEASE_VERSION_MINOR                                   3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(SERVICESTBM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible SERVICESTBM_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(SERVICESTBM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible SERVICESTBM_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_ServiceStbM, SERVICESTBM_VAR) ServiceStbM;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, SERVICESTBM_CODE) module_ServiceStbM::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, SERVICESTBM_CONST,       SERVICESTBM_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICESTBM_CONFIG_DATA, SERVICESTBM_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == ServiceStbM_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstServiceStbM_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == ServiceStbM_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == ServiceStbM_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == ServiceStbM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICESTBM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SERVICESTBM_CODE) module_ServiceStbM::DeInitFunction(
+   void
+){
+#if(STD_ON == ServiceStbM_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceStbM_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == ServiceStbM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICESTBM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SERVICESTBM_CODE) module_ServiceStbM::MainFunction(
+   void
+){
+#if(STD_ON == ServiceStbM_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceStbM_InitCheck)
+   }
+   else{
+#if(STD_ON == ServiceStbM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICESTBM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
