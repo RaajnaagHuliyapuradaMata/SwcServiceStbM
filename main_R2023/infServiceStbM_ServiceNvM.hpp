523c3fdf37e02a833cb69cb1d3cbdd25a8b2f541
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:49:50 +0100

    Update: AUTOSAR basic software service module StbM

---
+++ main_R2023/infServiceStbM_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceStbM_ServiceNvM.hpp                                     */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICESTBM_AR_RELEASE_VERSION_MAJOR                              4
+#define CONSTSERVICESTBM_AR_RELEASE_VERSION_MINOR                              3
+
+#define Const_DefaultInit_ConstServiceStbM                                                                               \
+   ,  {CONSTSERVICESTBM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICESTBM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
